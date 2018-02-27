566f2588d6853828bb9b129c57157df3bd2e547b
Mattias Gees <mattias.gees@gmail.com>
2018-02-27 17:46:12 +0100

    Add vpc config to lambda

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -5,21 +5,26 @@ data "archive_file" "es_cleanup_lambda" {
 }
 
 resource "aws_lambda_function" "es_cleanup" {
-    filename         = "${path.module}/es-cleanup.zip"
-    function_name    = "${var.prefix}es-cleanup"
-    timeout          = 300
-    runtime          = "python${var.python_version}"
-    role             = "${aws_iam_role.role.arn}"
-    handler          = "es-cleanup.lambda_handler"
-    source_code_hash = "${data.archive_file.es_cleanup_lambda.output_base64sha256}"
+  filename         = "${path.module}/es-cleanup.zip"
+  function_name    = "${var.prefix}es-cleanup"
+  timeout          = 300
+  runtime          = "python${var.python_version}"
+  role             = "${aws_iam_role.role.arn}"
+  handler          = "es-cleanup.lambda_handler"
+  source_code_hash = "${data.archive_file.es_cleanup_lambda.output_base64sha256}"
 
-    environment {
-        variables = {
-            es_endpoint  = "${var.es_endpoint}"
-            index        = "${var.index}"
-            delete_after = "${var.delete_after}"
-            index_format = "${var.index_format}"
-            sns_alert    = "${var.sns_alert}"
-        }
+  environment {
+    variables = {
+      es_endpoint  = "${var.es_endpoint}"
+      index        = "${var.index}"
+      delete_after = "${var.delete_after}"
+      index_format = "${var.index_format}"
+      sns_alert    = "${var.sns_alert}"
     }
+  }
+
+  vpc_config {
+    subnet_ids         = ["${var.subnet_ids}"]
+    security_group_ids = ["${aws_security_group.lambda.*.id}"]
+  }
 }
