cfdc6b6d3f8d2c831dddd4b6d07904d33f95926e
Giulio Calzolari <9049490+giuliocalzolari@users.noreply.github.com>
2020-04-22 09:02:08 +0200

    fix-pr-37 (#39)
    Co-authored-by: Giulio Calzolari <giuliocalzolari@users.noreply.github.com>

--- es_cleanup_test.py
+++ es_cleanup_test.py
@@ -32,6 +32,66 @@ class TestShouldDelete(unittest.TestCase):
         self.assertFalse(tuple[0])
         self.assertTrue("matches skip condition" in tuple[1])
 
+    def test_should_skip_indes_2(self):
+        tuple = decider.should_delete({"index": ".kibana_1"})
+        self.assertFalse(tuple[0])
+        self.assertTrue("matches skip condition" in tuple[1])
+
+
+
+
+
+decider2 = es_cleanup.DeleteDecider(delete_after=4,
+                                   idx_format='%Y.%m.%d',
+                                   idx_regex='app[1-2].*|k8s.*',
+                                   skip_idx_regex='kibana.*',
+                                   today=datetime.date(2019, 12, 19))
+
+
+class TestShouldDelete2(unittest.TestCase):
+    def test_should_be_deleted(self):
+        tuple = decider2.should_delete({"index": "k8s-2019.12.14"})
+        self.assertTrue(tuple[0])
+
+    def test_should_not_be_deleted(self):
+        tuple = decider2.should_delete({"index": "k8s-2019.12.15"})
+        self.assertFalse(tuple[0])
+
+    def test_should_be_deleted_app1(self):
+        tuple = decider2.should_delete({"index": "app1-2019.12.14"})
+        self.assertTrue(tuple[0])
+
+    def test_should_not_be_deleted_app1(self):
+        tuple = decider.should_delete({"index": "app1-2019.12.15"})
+        self.assertFalse(tuple[0])
+
+    def test_should_be_deleted_app2(self):
+        tuple = decider2.should_delete({"index": "app2-2019.12.14"})
+        self.assertTrue(tuple[0])
+
+    def test_should_not_be_deleted_app2(self):
+        tuple = decider2.should_delete({"index": "app2-2019.12.15"})
+        self.assertFalse(tuple[0])
+
+    def test_should_not_be_deleted_app3(self):
+        tuple = decider2.should_delete({"index": "app3-2019.12.14"})
+        self.assertFalse(tuple[0])
+
+    def test_should_raise_value_error(self):
+        with self.assertRaises(ValueError):
+            decider2.should_delete({"index": "k8s-2019-12-15"})
+
+    def test_should_skip_indes(self):
+        tuple = decider2.should_delete({"index": ".kibana"})
+        self.assertFalse(tuple[0])
+        self.assertTrue("matches skip condition" in tuple[1])
+
+    def test_should_skip_indes_2(self):
+        tuple = decider2.should_delete({"index": ".kibana_1"})
+        self.assertFalse(tuple[0])
+        self.assertTrue("matches skip condition" in tuple[1])
+
+
 
 if __name__ == '__main__':
     unittest.main()
